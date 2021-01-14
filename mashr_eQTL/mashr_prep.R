c9e13064cab04d124bc208db4a2703d5cef5a7b5
resztak <ga6092@wayne.edu>
2021-01-14 14:39:28 -0500

    added zipping all the input files

---
+++ mashr_eQTL/mashr_prep.R
@@ -0,0 +1,48 @@
+# this script fits mash on all the preprocessed SCAIP FastQTL eQTL mapping results and generates the input for mashr (pvalues,SEs)
+# based on ../../eQTL/FastQTL/nominals/mashr/mashr_prep.R
+# 1/13/2021 JR
+
+library(ashr)
+library(data.table)
+
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args)>0){
+      dataset <- args[1]
+    }
+
+pcs = 3
+
+eQTL_dir <- "../eQTL_mapping/eQTL_output/"
+
+# read in, grab effect estimate and its standard error
+m <- fread(paste0(eQTL_dir, dataset,".GEPC",pcs,".nominals.eQTL.txt.gz"),sep=" ")
+colnames(m) <- c("ENSG","varID", "distance", "pvalue", "slope")
+# add unique SNP identifier:
+m$uniqID <- paste0(m$ENSG,"_",m$varID)
+# calculate SE:
+m$z.abs=abs(qnorm(m$pvalue/2))
+m$SE = abs(m$slope)/m$z.abs
+# calculate lfsr:
+ashr=ash(m$slope,m$SE)
+m$lfsr <- ashr$result$lfsr
+# save the slope and the SE in separate files:
+slope <- m[,c("uniqID","slope")]
+colnames(slope)[2] <- dataset
+write.table(slope, paste0("input/", dataset, "_slope.txt"), sep="\t", col.names=T, row.names=F, quote=F)
+SE <- m[,c("uniqID","SE")]
+colnames(SE)[2] <- dataset
+write.table(SE, paste0("input/", dataset, "_SE.txt"), sep="\t", col.names=T, row.names=F, quote=F)
+# also save pvalues to select "strong" signals for estimating covariances step:
+pvals <- m[,c("uniqID","pvalue")]
+colnames(pvals)[2] <- dataset
+# save lfsr to use instead of pvalues
+write.table(pvals, paste0("input/", dataset, "_pvalue.txt"), sep="\t", col.names=T, row.names=F, quote=F)
+lfsr <- m[,c("uniqID","lfsr")]
+colnames(lfsr)[2] <- dataset
+write.table(lfsr, paste0("input/", dataset, "_lfsr.txt"), sep="\t", col.names=T, row.names=F, quote=F)
+
+# gzip all:
+system(paste0("for file in input/", dataset,"*txt ; do gzip -f '$file' > '$file.gz'; done"))
+
+### END 1/13/2021 JR
